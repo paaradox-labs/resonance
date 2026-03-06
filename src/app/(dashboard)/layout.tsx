3fd246236ce650c3f46bea44f30fe8717aaa4902
Mr. Paradox <avyas8927@gmail.com>
2026-03-06 22:12:57 +0530

    checking ci

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { cookies } from "next/headers";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { DashBoardSidebar } from "@/features/dashboard/components/dashboard-sidebar";
